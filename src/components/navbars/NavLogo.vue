9989b56d58b6bae95b0cc41a67de29f5972b58a4
Christian Labonté <cbonte99@gmail.com>
2022-04-03 16:41:02 -0400

    Added initial application navigation structure and sample dashboad page content

--- src/components/navbars/NavLogo.vue
+++ src/components/navbars/NavLogo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-shrink-0 flex items-center">
+  <div class="flex-shrink-0 flex items-center" v-if="isMarketing">
     <img
       class="block lg:hidden h-8 w-auto"
       src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
@@ -11,11 +11,36 @@
       alt="Workflow"
     />
   </div>
+  <div :class="divCssClass" v-else>
+    <img
+      class="h-8 w-auto"
+      src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
+      alt="Workflow"
+    />
+  </div>
 </template>
 
 <script>
 export default {
   name: "NavLogo",
+  props: {
+    mobile: { type: Boolean, default: false },
+    mode: { type: String, default: "marketing" },
+  },
+  computed: {
+    isMarketing() {
+      return this.mode === "marketing";
+    },
+    isApplication() {
+      return this.mode === "application";
+    },
+    divCssClass() {
+      if (this.mobile) {
+        return "flex-shrink-0 flex items-center px-4";
+      }
+      return "flex items-center flex-shrink-0 px-6";
+    },
+  },
 };
 </script>
 
